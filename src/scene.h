c52f588071b4ee666bd84d5608c4b300b00769f2
luodong <luod1027@gmail.com>
2022-03-24 00:12:21 +0800

    perlin noise

--- src/scene.h
+++ src/scene.h
@@ -78,11 +78,12 @@ bvh_node two_spheres()
     return world;
 }
 
-bvh_node two_perlin_spheres() {
+bvh_node two_perlin_spheres()
+{
     hittable_list objects;
 
-    auto pertext = make_shared<noise_texture>();
-    objects.add(make_shared<sphere>(point3(0,-1000,0), 1000, make_shared<lambertian>(pertext)));
+    auto pertext = make_shared<noise_texture>(4);
+    objects.add(make_shared<sphere>(point3(0, -1000, 0), 1000, make_shared<lambertian>(pertext)));
     objects.add(make_shared<sphere>(point3(0, 2, 0), 2, make_shared<lambertian>(pertext)));
 
     bvh_node world(objects, 0, 1);
--- src/texture/noise_texture.h
+++ src/texture/noise_texture.h
@@ -8,9 +8,11 @@ class noise_texture : public texture
 {
 public:
     perlin noise;
+    double scale;
 
 public:
     noise_texture() {}
+    noise_texture(double sc) : scale(sc) {}
 
     virtual color value(double u, double v, const point3 &p) const override
     {
